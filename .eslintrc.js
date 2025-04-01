248c46f24949a59a621b947d4aa00f51537de0a2
Cédric <cedric.marinot@elosi.com>
2025-04-01 16:05:23 +0200

    MOC convenable (save - màj de eslint)

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+const OFF = 0;
+const WARNING = 1;
+const ERROR = 2;
+
+module.exports = {
+  "extends": [
+    "next/core-web-vitals",
+    "next/typescript"
+  ],
+  "rules": {
+    semi: [ERROR, 'always'],
+    'space-infix-ops': ERROR,
+    'no-multi-spaces': ERROR,
+    'class-methods-use-this': WARNING,
+    'arrow-parens': [ERROR, 'as-needed'],
+    '@typescript-eslint/no-unused-vars': ERROR,
+    // "no-restricted-imports": [
+    //   "error",
+    //   {
+    //     "patterns": ["./*", "../*"]
+    //   }
+    // ]
+  }
+}
